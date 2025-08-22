9e8f5185e15a18960314b1e86be8bdea5f20c16a
Breno-Lira <bmrl@cesar.school>
2025-08-22 17:38:01 -0300

    Troca de posições e finalizando

--- src/main/java/com/devprojeto/dslist/controllers/GameListController.java
+++ src/main/java/com/devprojeto/dslist/controllers/GameListController.java
@@ -4,13 +4,11 @@ package com.devprojeto.dslist.controllers;
 import com.devprojeto.dslist.dto.GameDTO;
 import com.devprojeto.dslist.dto.GameListDTO;
 import com.devprojeto.dslist.dto.GameMinDTO;
+import com.devprojeto.dslist.dto.ReplacementDTO;
 import com.devprojeto.dslist.services.GameListService;
 import com.devprojeto.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,6 +32,11 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable Long listId) {
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move (@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 
 
